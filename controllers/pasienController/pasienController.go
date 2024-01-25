4275eb966ebfcde661b1e8fd98ae5e864cb64628
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-01-25 23:09:03 +0700

    feat: initiate pasien route and controller

---
+++ controllers/pasienController/pasienController.go
@@ -0,0 +1,22 @@
+package pasiencontroller
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Index(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("Api works!")
+}
+
+func Add(response http.ResponseWriter, request *http.Request) {
+	
+}
+
+func Edit(response http.ResponseWriter, request *http.Request) {
+	
+}
+
+func Delete(response http.ResponseWriter, request *http.Request) {
+	
+}
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	pasiencontroller "github.com/reynaldineo/CRUD-Golang-Native/controllers/pasienController"
+)
 
 func main()  {
-	fmt.Println("Hello World its rey")
+
+	http.HandleFunc("/", pasiencontroller.Index)
+	http.HandleFunc("/pasien", pasiencontroller.Index)
+	http.HandleFunc("/pasien/index", pasiencontroller.Index)
+	http.HandleFunc("/pasien/add", pasiencontroller.Add)
+	http.HandleFunc("/pasien/edit", pasiencontroller.Edit)
+	http.HandleFunc("/pasien/delete", pasiencontroller.Delete)
+
+	http.ListenAndServe(":3000", nil)
 }
