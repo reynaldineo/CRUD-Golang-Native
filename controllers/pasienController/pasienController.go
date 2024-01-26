dadd1aeb260289ec3a0eaf78d19285f58e47f8e1
reynaldi neo <reynaldineoramadhani@gmail.com>
2024-01-26 23:31:37 +0700

    feat: add view for pasien index and add

--- controllers/pasienController/pasienController.go
+++ controllers/pasienController/pasienController.go
@@ -2,14 +2,31 @@ package pasiencontroller
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func Index(response http.ResponseWriter, request *http.Request) {
-	fmt.Println("Api works!")
+	fmt.Println("api index");
+
+	temp, err := template.ParseFiles("views/pasien/index.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(response, nil)
 }
 
 func Add(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("api add");
+
+	if request.Method == http.MethodGet {
+			temp, err := template.ParseFiles("views/pasien/add.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, nil)
+	}
+
 	
 }
 
